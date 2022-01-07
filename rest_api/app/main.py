a5f8c7fc95693ef41f341faedf31576df9be9e0e
Atef Arfaoui <arfaoui47@gmail.com>
2022-01-07 23:58:05 +0100

    Update trigger_dag endpoint

--- rest_api/app/main.py
+++ rest_api/app/main.py
@@ -1,8 +1,12 @@
 from typing import Any, Optional
+import os
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel, Json
+from mwaa_cli import send_request, get_cli_parameters
 
+
+ENV = os.environ.get('ENV', 'dev')
 
 class TiggerDAGConfig(BaseModel):
     dag_id: str
@@ -15,8 +19,9 @@ app = FastAPI()
 # https://airflow.apache.org/docs/apache-airflow/2.0.2/cli-and-env-variables-ref.html#trigger
 @app.post("/trigger_dag")
 def run_cmd(dag_parms: TiggerDAGConfig):
-    print(dag_parms)
-    return {'dag_id': dag_parms.dag_id, 'run_id': dag_parms.run_id}
+    cli_parameters = get_cli_parameters(ENV)
+    std_out, std_err = send_request(cli_parameters, dag_parms.json())
+    return {'std_out': std_out, 'std_err': std_err}
 
 
 if __name__ == '__main__':
